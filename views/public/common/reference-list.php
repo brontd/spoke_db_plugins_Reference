ee3e0a94cad38e384ebca438805d8efaf18bceea
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2020-04-14 17:28:28 +0200

    Added option to hide/show reference count, cleaned up code

--- views/public/common/reference-list.php
+++ views/public/common/reference-list.php
@@ -1,87 +1,94 @@
 <?php
 if (count($references)):
-    $queryType = get_option('reference_query_type') == 'contains' ? 'contains' : 'is+exactly';
-    // Dublin Core Title is always 50.
-    $referenceId = $slugData['type'] == 'Element' ? $slugData['id'] : 50;
-    // Prepare and display skip links.
-    if ($options['skiplinks']):
-        // Get the list of headers.
-        $letters = array('number' => false) + array_fill_keys(range('A', 'Z'), false);
-        foreach ($references as $reference => $referenceData):
-            $first_char = function_exists('mb_substr') ? mb_substr($reference, 0, 1) : substr($reference, 0, 1);
-            if (strlen($first_char) == 0 || preg_match('/\W|\d/u', $first_char)):
-                $letters['number'] = true;
-            else:
-                $letters[strtoupper($first_char)] = true;
-            endif;
-        endforeach;
-        $pagination_list = '<ul class="pagination_list">';
-        foreach ($letters as $letter => $isSet):
-            $letterDisplay = $letter == 'number' ? '#0-9' : $letter;
-            if ($isSet):
-                $pagination_list .= sprintf('<li class="pagination_range"><a href="#%s">%s</a></li>', $letter, $letterDisplay);
-            else:
-                $pagination_list .= sprintf('<li class="pagination_range"><span>%s</span></li>', $letterDisplay);
-            endif;
-        endforeach;
-        $pagination_list .= '</ul>';
-    ?>
+	$reference_hide_empty = (boolean) get_option('reference_hide_empty');
+	$reference_show_count = (boolean) get_option('reference_show_count');
+	$queryType = get_option('reference_query_type') == 'contains' ? 'contains' : 'is+exactly';
+	// Dublin Core Title is always 50.
+	$referenceId = $slugData['type'] == 'Element' ? $slugData['id'] : 50;
+	// Prepare and display skip links.
+	if ($options['skiplinks']):
+		// Get the list of headers.
+		$letters = array('number' => false) + array_fill_keys(range('A', 'Z'), false);
+		foreach ($references as $reference => $referenceData):
+			$first_char = function_exists('mb_substr') ? mb_substr($reference, 0, 1) : substr($reference, 0, 1);
+			if (strlen($first_char) == 0 || preg_match('/\W|\d/u', $first_char)):
+				$letters['number'] = true;
+			else:
+				$letters[strtoupper($first_char)] = true;
+			endif;
+		endforeach;
+		$pagination_list = '<ul class="pagination_list">';
+		foreach ($letters as $letter => $isSet):
+			$letterDisplay = $letter == 'number' ? '#0-9' : $letter;
+			if ($isSet):
+				$pagination_list .= sprintf('<li class="pagination_range"><a href="#%s">%s</a></li>', $letter, $letterDisplay);
+			else:
+				$pagination_list .= sprintf('<li class="pagination_range"><span>%s</span></li>', $letterDisplay);
+			endif;
+		endforeach;
+		$pagination_list .= '</ul>';
+	?>
 <div class="pagination reference-pagination" id="pagination-top">
-    <?php echo $pagination_list; ?>
+	<?php echo $pagination_list; ?>
 </div>
-    <?php endif; ?>
+	<?php endif; ?>
 
 <div id="reference-headings">
-    <?php
-    $linkSingle = (boolean) get_option('reference_link_to_single');
-    $current_heading = '';
-    $current_id = '';
-    foreach ($references as $reference => $referenceData):
-        // Add the first character as header if wanted.
-        if ($options['headings']):
-            $first_char = function_exists('mb_substr') ? mb_substr($reference, 0, 1) : substr($reference, 0, 1);
-            if (strlen($first_char) == 0 || preg_match('/\W|\d/u', $first_char)) {
-                $first_char = '#0-9';
-            }
-            $current_first_char = strtoupper($first_char);
-            if ($current_heading !== $current_first_char):
-                $current_heading = $current_first_char;
-                $current_id = $current_heading === '#0-9' ? 'number' : $current_heading;
-    ?>
-    <h3 class="reference-heading" id="<?php echo $current_id; ?>"><?php echo $current_heading; ?></h3>
-    <?php
-            endif;
-        endif;
-    ?>
+	<ul class="references" style="list-style-type:none"> 
+	<?php
+	$linkSingle = (boolean) get_option('reference_link_to_single');
+	$current_heading = '';
+	$current_id = '';
+	foreach ($references as $reference => $referenceData):
+		// Add the first character as header if wanted.
+		if ($options['headings']):
+			$first_char = function_exists('mb_substr') ? mb_substr($reference, 0, 1) : substr($reference, 0, 1);
+			if (strlen($first_char) == 0 || preg_match('/\W|\d/u', $first_char)) {
+				$first_char = '#0-9';
+			}
+			$current_first_char = strtoupper($first_char);
+			if ($current_heading !== $current_first_char):
+				$current_heading = $current_first_char;
+				$current_id = $current_heading === '#0-9' ? 'number' : $current_heading;
+	?>
+	<li><h3 class="reference-heading" id="<?php echo $current_id; ?>"><?php echo $current_heading; ?></h3></li>
+	<?php
+			endif;
+		endif;
+	?>
 
-    <p class="reference-record">
-        <?php if (empty($options['raw'])):
-            if ($linkSingle && $referenceData['count'] === 1):
-                $record = get_record_by_id('Item', $referenceData['record_id']);
-                echo link_to($record, null, $reference);
-            else:
-                $url = 'items/browse?';
-                if ($slugData['type'] == 'ItemType'):
-                    $url .= 'type=' . $slugData['id'] . '&amp;';
-                endif;
-                $url .= sprintf('advanced[0][element_id]=%s&amp;advanced[0][type]=%s&amp;advanced[0][terms]=%s',
-                    $referenceId, $queryType, urlencode($reference));
-                echo '<a href="' . url($url) . '">' . $reference . '</a>';
-                // Can be null when references are set directly.
-                if ($referenceData['count']):
-                    echo ' (' . $referenceData['count'] . ')';
-                endif;
-            endif;
-        else:
-            echo $reference;
-        endif; ?>
-    </p>
-    <?php endforeach; ?>
+	<p class="reference-record">
+		<?php if (empty($options['raw'])):
+			if ($linkSingle && $referenceData['count'] === 1):
+				$record = get_record_by_id('Item', $referenceData['record_id']);
+				echo '<li>' . link_to($record, null, $reference) . '</li>';
+			else:
+				if (!$reference_hide_empty || $this->reference()->count($slug) > 0):
+					$url = 'items/browse?';
+					if ($slugData['type'] == 'ItemType'):
+						$url .= 'type=' . $slugData['id'] . '&amp;';
+					endif;
+					$url .= sprintf('advanced[0][element_id]=%s&amp;advanced[0][type]=%s&amp;advanced[0][terms]=%s',
+						$referenceId, $queryType, urlencode($reference));
+					echo '<li><a href="' . url($url) . '">' . $reference . '</a>';
+					// Can be null when references are set directly.
+					if ($reference_show_count && $referenceData['count']) echo ' (' . $referenceData['count'] . ')';
+					echo '</li>';
+				endif;
+			endif;
+		else:
+			echo $reference;
+		endif; ?>
+	</p>
+	<?php endforeach; ?>
+	</ul>
 </div>
 
-    <?php if ($options['skiplinks']): ?>
+	<?php if ($options['skiplinks']): ?>
+	
 <div class="pagination reference-pagination" id="pagination-bottom">
-    <?php echo $pagination_list; ?>
+	<?php echo $pagination_list; ?>
 </div>
-    <?php endif;
+	
+	<?php endif;
 endif;
