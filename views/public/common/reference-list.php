f2a6e668f7030b7a5a309ca9aea39c3bbd063536
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2020-07-09 16:12:05 +0200

    Improved layout

--- views/public/common/reference-list.php
+++ views/public/common/reference-list.php
@@ -2,6 +2,8 @@
 if (count($references)):
 	$reference_hide_empty = (bool) get_option('reference_hide_empty');
 	$reference_show_count = (bool) get_option('reference_show_count');
+	$reference_link_columns = intval(get_option('reference_link_columns'));
+	if ($reference_link_columns == 0) $reference_link_columns = 1;
 	$queryType = get_option('reference_query_type') == 'contains' ? 'contains' : 'is+exactly';
 	// Dublin Core Title is always 50.
 	$referenceId = $slugData['type'] == 'Element' ? $slugData['id'] : 50;
@@ -12,6 +14,7 @@ if (count($references)):
 		uksort($references, array($collator, 'compare'));
 		$alphabet = (get_option('reference_list_alphabet') != '' ? explode(' ', get_option('reference_list_alphabet')) : array_fill_keys(range('A', 'Z'), false)); 
 		$letters = array('number' => false) + array_fill_keys($alphabet, false);
+
 		foreach ($references as $reference => $referenceData):
 			$first_char = mb_substr($reference, 0, 1, 'UTF-8');
 			if (strlen($first_char) == 0 || preg_match('/\W|\d/u', $first_char)):
@@ -21,7 +24,9 @@ if (count($references)):
 				$letters[$first_char] = true;
 			endif;
 		endforeach;
+
 		$pagination_list = '<ul class="pagination_list">';
+
 		foreach ($letters as $letter => $isSet):
 			$letterDisplay = ($letter == 'number' ? '#0-9' : $letter);
 			if ($isSet):
@@ -37,6 +42,7 @@ if (count($references)):
 				);
 			endif;
 		endforeach;
+
 		$pagination_list .= '</ul>';
 	?>
 <div class="pagination reference-pagination" id="pagination-top">
@@ -45,11 +51,13 @@ if (count($references)):
 	<?php endif; ?>
 
 <div id="reference-headings">
-	<ul class="references" style="list-style-type:none"> 
+	<div class="references" style="list-style-type:none; column-count: <?php echo $reference_link_columns; ?>"> 
 	<?php
 	$linkSingle = (bool) get_option('reference_link_to_single');
 	$current_heading = '';
 	$current_id = '';
+	$ul_is_open = false;
+
 	foreach ($references as $reference => $referenceData):
 		// Add the first character as header if wanted.
 		if ($options['headings']):
@@ -61,18 +69,20 @@ if (count($references)):
 			if ($current_heading !== $current_first_char):
 				$current_heading = $current_first_char;
 				$current_id = ($current_heading === '#0-9' ? 'number' : $current_heading);
+				if ($ul_is_open) echo "</ul>\n";
 	?>
-	<li><h3 class="reference-heading" id="<?php echo $current_id; ?>"><?php echo $current_heading; ?></h3></li>
+	<ul class="reference-list"><li><h3 class="reference-heading" id="<?php echo $current_id; ?>"><?php echo $current_heading; ?></h3></li>
 	<?php
+				$ul_is_open = true;
 			endif;
 		endif;
 	?>
 
-	<p class="reference-record">
+	<li class="reference-record">
 		<?php if (empty($options['raw'])):
 			if ($linkSingle && $referenceData['count'] === 1):
 				$record = get_record_by_id('Item', $referenceData['record_id']);
-				echo '<li>' . link_to($record, null, $reference) . ' (1)</li>';
+				echo link_to($record, null, $reference) . ' (1)';
 			else:
 				if (!$reference_hide_empty || $this->reference()->count($slug) > 0):
 					$url = 'items/browse?';
@@ -85,22 +95,21 @@ if (count($references)):
 						$queryType,
 						urlencode($reference)
 					);
-					echo '<li><a href="' . url($url) . '">' . $reference . '</a>';
+					echo '<a href="' . url($url) . '">' . $reference . '</a>';
 					// Can be null when references are set directly.
 					if ($reference_show_count && $referenceData['count']) echo ' (' . $referenceData['count'] . ')';
-					echo '</li>';
 				endif;
 			endif;
 		else:
 			echo $reference;
 		endif; ?>
-	</p>
+	</li>
 	<?php endforeach; ?>
-	</ul>
+	</div>
 </div>
 
 	<?php if ($options['skiplinks']): ?>
-<div class="pagination reference-pagination" id="pagination-bottom">
+<div class="pagination reference-pagination" style="margin-top: 1em" id="pagination-bottom">
 	<?php echo $pagination_list; ?>
 </div>
 	<?php endif;
