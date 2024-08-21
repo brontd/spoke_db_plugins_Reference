3681441e80c2f1c6cf5dbd4b0f68f5a1ed43da1b
bront <bront.davis@uky.edu>
2024-08-21 13:36:23 -0400

    Update browse.php

--- views/public/index/browse.php
+++ views/public/index/browse.php
@@ -1,17 +1,9 @@
 <?php
-if (empty($types) || count($types) > 1) {
-    $pageTitle = __('References');
-} elseif ($types['Element']) {
-    $pageTitle = __('Metadata');
-} else {
-    $pageTitle = __('Item Types');
-}
+$pageTitle = __('References');
 echo head(array(
     'title' => $pageTitle,
     'bodyclass' => 'reference',
 ));
-$reference_hide_empty = (bool) get_option('reference_hide_empty');
-$reference_show_count = (bool) get_option('reference_show_count');
 ?>
 <div id="primary">
     <h1><?php echo $pageTitle; ?></h1>
@@ -19,10 +11,25 @@ $reference_show_count = (bool) get_option('reference_show_count');
         <?php echo public_nav_items(); ?>
     </nav>
     <?php if (empty($types)): ?>
-        <p><?php echo __('No reference available.'); ?></p>
+        <p><?php echo __('No references available.'); ?></p>
     <?php else: ?>
-    <div id="reference-headings">
-        <ul class="reference-list" style="margin-top: 16px;">
+        <?php
+    if (count($types) == 1): ?>
+        <ul class='references'>
+        <?php foreach ($references as $slug => $slugData): ?>
+            <li><?php
+                echo sprintf('<a href="%s" title="%s">%s (%d)</a>',
+                    html_escape(url(array('slug' => $slug), 'reference_list')),
+                    __('Browse %s', $slugData['label']),
+                    $slugData['label'],
+                    $this->reference()->count($slug)
+                );
+            ?>
+            </li>
+        <?php endforeach; ?>
+        </ul>
+    <?php else: ?>
+        <ul class='references'>
         <?php
         // References are ordered: Item Types, then Elements.
         $type = null;
@@ -32,43 +39,26 @@ $reference_show_count = (bool) get_option('reference_show_count');
             if ($changedType):
                 if ($first):
                     $first = false;
-                else:
-                    echo "</ul>";
-                                echo "<ul class='reference-list' style='margin-top: 16px;'>";
-                endif;
-            ?>
-            <li>
-            <?php
-                echo '<h3 class="reference-heading">' . ($slugData['type'] == 'ItemType' ?  __('Main Item Types') : __('Metadata')) . '</h3>';
+                else: ?>
+                    </ul></li>
+                <?php endif; ?>
+            <li><?php
+                echo $slugData['type'] == 'ItemType' ?  __('Main Types of Items') : __('Metadata');
                 $type = $slugData['type'];
+            ?><ul>
+            <?php endif; ?>
+            <li><?php
+                echo sprintf('<a href="%s" title="%s">%s (%d)</a>',
+                    html_escape(url(array('slug' => $slug), 'reference_list')),
+                    __('Browse %s', $slugData['label']),
+                    $slugData['label'],
+                    $this->reference()->count($slug)
+                );
             ?>
             </li>
-        <?php endif; ?>
-
-        <?php
-            if (!$reference_hide_empty || $this->reference()->count($slug) > 0) {
-                echo '<li class="reference-record">';
-                if ($reference_show_count) {
-                    echo sprintf(
-                        '<a href="%s" title="%s">%s</a> (%d)',
-                        html_escape(url(array('slug' => $slug), 'reference_list')),
-                        __('Browse %s', $slugData['label']),
-                        __($slugData['label']),
-                        $this->reference()->count($slug)
-                    );
-                } else {
-                    echo sprintf(
-                        '<a href="%s" title="%s">%s</a>',
-                        html_escape(url(array('slug' => $slug), 'reference_list')),
-                        __('Browse %s', $slugData['label']),
-                        __($slugData['label'])
-                    );
-                }
-                echo '</li>';
-            }
-        ?>
         <?php endforeach; ?>
         </ul></li>
+    <?php endif; ?>
     </ul>
     <?php endif; ?>
 </div>
